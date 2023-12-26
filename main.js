df8158ba964463a18c90e495b63c5fe38cdc3b37
VividerAphid <trever.kress@gmail.com>
2023-12-26 01:20:45 -0500

    Completed engine setup

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 function processInput(inp){
     inp = inp.toLowerCase();
+    inp = inp.replace(/\s+/g, '');
+    let found = false;
+    for(let r in responses){
+        if(inp == r){
+            found = true;
+            respDiv.innerHTML = responses[r];
+            break;
+        }
+    }
+    if(!found){
+        let pick = Math.floor(Math.random()*invalidInpResponses.length);
+        respDiv.innerHTML = invalidInpResponses[pick];
+    }
+}
+
+function speakClick(){
+    let input = txtArea.value;
+    txtArea.value = "";
+    processInput(input);
 }
--- responses.js
+++ responses.js
@@ -1,3 +1,11 @@
 var responses = {
-    
+    frog: "Frogs of two, will not do, frogs of three, not for thee, frogs of four, shows you more.",
+    hemmegoogoo: "With...?",
+    googoohemme: "With.......?"
 };
+
+var invalidInpResponses = [
+    "I have no idea what you are even saying.",
+    "Try talking.",
+    "What?"
+];
