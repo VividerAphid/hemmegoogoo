c41945546a0181bb7168b98038b701d392be2fa7
VividerAphid <trever.kress@gmail.com>
2023-12-26 02:44:47 -0500

    Added clues and responses

--- responses.js
+++ responses.js
@@ -1,7 +1,17 @@
 var responses = {
-    frog: "Frogs of two, will not do, frogs of three, not for thee, frogs of four, shows you more.",
+    hi: "Why hello.",
+    hello: "Why hello.",
+    higoogoo: "Why hello.",
+    hellogoogoo: "Why hello.",
     hemmegoogoo: "With...?",
-    googoohemme: "With.......?"
+    googoohemme: "With.......?",
+    begin: "First, lets find you a bag... Have you tried the house down the street? I sense a bag there waiting for you. Tell me what you find in it.",
+    fox: "So you found the fox, now you need a snail. Hope you levelled up in foraging. Try outside by the shed.",
+    snail: "Now you need a fanny pack frog, of course. Should be one nearby.",
+    frog: "You thought it would be that easy? Try again.",
+    a22x: "My Wisdom tells me there is a combination lock containing further info for your quest. Check inside in the kitchen.",
+    lockemaste: "Frogs of two, will not do. Frogs of three, shall mislead thee. Frogs of four will show you more. A helpful hornet can get you started.",
+    boat: "The pink frog lies in wait where the coats stay",
 };
 
 var invalidInpResponses = [
