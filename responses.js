f93a598d40ded77a5c7140d5b37750518fde76d1
VividerAphid <trever.kress@gmail.com>
2023-12-26 13:36:43 -0500

    Responses finished

--- responses.js
+++ responses.js
@@ -12,6 +12,26 @@ var responses = {
     a22x: "My Wisdom tells me there is a combination lock containing further info for your quest. Check inside in the kitchen.",
     lockemaste: "Frogs of two, will not do. Frogs of three, shall mislead thee. Frogs of four will show you more. A helpful hornet can get you started.",
     boat: "The pink frog lies in wait where the coats stay",
+    frog1: "You trust my Wisdom much to follow me blindly, go consult with the evil cicada in the laundry room. Leave the clue in the speaking mouth.",
+    d3ad: "Under the stairs you will find the yellow frog lies in ambush.",
+    frog2: "Nope.",
+    froggg: "What's cooking in the microwave?",
+    roach: "Third frog is in the fire.",
+    froog3: "My last brethren is in a half bath cabinet.",
+    frogo: "I sense you will need the nice cicadas help too.",
+    pots: "Find the potion set upstairs.",
+    evil: "Tell me...What color do you get from mixing Concentrated Een secretions and Potent Death?",
+    green: "Good. And Liquid Googoo Root with Potent Death?",
+    purple: "Excellent. Find the tic tac toe board up here.",
+    boiscutter: "How was round 4 won?",
+    ac1: "How was round 2 won?",
+    fc4: "The total of red and blue pieces, verbose.",
+    eleven: "Nice, Find the Council of Ground and Grass in the shed.",
+    theholybluebug: "To find the Holy Blue Bug, locate the Great Council of the Wings where the smell is the worst.",
+    cookhimgood: "Check the oven...",
+    ohboy: "The mug is in the fridge...",
+    mugmugmugmug: "You spent all this time looking for a mug. Congrats."
+
 };
 
 var invalidInpResponses = [
